b2c05278aa014d467e9cfca0296876078183f0da
makaaaaaaowo <131304428+makaaaaaaowo@users.noreply.github.com>
2023-11-21 15:34:12 -0300

    Add files via upload

---
+++ crm/static/js/app.js
@@ -0,0 +1,14 @@
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display = "none";
+
+
+}, 5000);
+
+
+
---
+++ crm/webapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Record(models.Model):
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    email = models.CharField(max_length=100)
+
+    phone = models.CharField(max_length=20)
+
+    address = models.CharField(max_length=255)
+
+    city = models.CharField(max_length=255)
+
+    province = models.CharField(max_length=200)
+
+    country = models.CharField(max_length=150)
+
+
+    def __str__(self):
+
+        return f"{self.first_name} {self.last_name}"
